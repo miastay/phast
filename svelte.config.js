fbdeb1ff3470a2f87870706940a3c6709f5a81e6
Mia Taylor <miastaylor1@gmail.com>
2024-02-02 16:04:11 -0800

    reorder for build

---
+++ svelte.config.js
@@ -0,0 +1,24 @@
+import adapter from "@sveltejs/adapter-static"; 
+import preprocess from 'svelte-preprocess';
+// was "@sveltejs/adapter-auto"
+
+const dev = "production" === "development";
+
+/** @type {import(""@sveltejs/kit").Config} */
+const config = {
+    kit: {
+        adapter: adapter({
+            pages: "docs",
+            assets: "docs"
+        }),
+        paths: {
+            // change below to your repo name
+            base: dev ? """ : "/your-repo-name",
+        },
+        // hydrate the <div id="svelte"> element in src/app.html
+        target: "#svelte"
+    },
+    preprocess: preprocess({ })
+};
+
+export default config;
