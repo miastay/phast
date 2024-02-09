2f950776793b0f5f533bd8e949d7893345bb8ea6
Mia Taylor <miastaylor1@gmail.com>
2024-02-09 01:48:16 -0800

    secure requests

--- src/util/geocoder.js
+++ src/util/geocoder.js
@@ -2,7 +2,7 @@
 const openweather_api_key = "7e5587b668bcd0d0c03ea54bd2d9e0fe"
 
 export async function reverseGeocode(lat, lng, limit = 1) {
-    return fetch(`http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lng}&limit=${limit}&appid=${openweather_api_key}`)
+    return fetch(`https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lng}&limit=${limit}&appid=${openweather_api_key}`)
     .then((response) => response.json())
     .then((data) => { return data[0]?.name ?? "Unknown"; })
 }
